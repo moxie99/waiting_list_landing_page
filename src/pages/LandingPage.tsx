e13a5a2f38ec3c037192e24c1b17da42ee68bfc2
Adeolu David <adeolusegun99@gmail.com>
2025-08-07 13:58:30 +0100

    link removed

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { WaitingListForm } from '@/components/WaitingListForm'
 import { motion } from 'framer-motion'
